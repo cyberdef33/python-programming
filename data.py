934230ecf9f8926082e51b65ac6e2f7811740c48
Cyber Tech <90408133+cyberdef33@users.noreply.github.com>
2022-07-06 00:16:43 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,60 @@
+# Data type python
+# Numeric data type
+# sequence data type
+# dictionary
+# set
+
+a=5
+print(a,type(a)) 
+# a=5 int type 
+b=2.2
+print(b,type(b))
+# b=2.2 float type
+
+s= 2+5j
+print(s,type(s))
+# s=2+5j complex data type
+
+# String
+
+w='Hello world'
+print(w,type(w))
+
+s= '''
+
+   welcome to cyber security
+    Team 
+   '''
+print(s)
+
+# List
+
+x=[1,'hacker',50,2.2]
+
+print(x,type(x))
+
+# Tuple
+
+t=(10,'hello',20,)
+print(t,type(t))
+
+
+# Dictionary
+
+d= {
+    'name':'super'
+}
+ 
+print(d,type(d))
+
+# set 
+
+my_number={10,20,40,50,50}
+print(my_number)
+
+
+
+
+
+
+
