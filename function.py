85faea977049e670bd16aad06f05d9dbdfc00d5c
Cyber Tech <90408133+cyberdef33@users.noreply.github.com>
2022-07-10 11:42:34 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,41 @@
+# String Fundtion
+# lower()
+# upper()
+# title()
+# capitalize()
+
+# x="Hacker world Team"
+# n=x.lower()
+# print(n)
+# 
+# n=x.upper()
+# print(n)
+# 
+# n=x.title()
+# print(n)
+# 
+# n=x.capitalize()
+# print(n)
+# 
+
+
+# find()
+# index()
+# isalpha()
+# isdigit()
+# isalnum()
+# 
+
+
+n= "Security12 Team Testing"
+print(n.find('e'))
+
+x="234"
+print(x.isdigit())
+
+a="hacker123"
+print(a.isalnum())
+
+
+b="cyber"
+print(b.isalpha())
