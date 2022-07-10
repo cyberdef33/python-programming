34c5a34f3a5cd8d7c5c1cd18cf72aa8cc59bab8b
Cyber Tech <90408133+cyberdef33@users.noreply.github.com>
2022-07-10 11:23:05 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,51 @@
+
+#  for loop statement
+for n in range(5):
+         print(n)
+
+for a in range(1,11):
+  print('5*',a,'=',5*a)
+
+
+
+# while loop statement
+
+a=10
+
+while a>1:
+    print(a,"Hello")
+    a=a-1
+    print(a)
+
+# String python
+
+w='world hacker'
+print(w[-1])
+# Right to Left
+print(w[0:3])
+# Left to Right
+print(w[0::2])
+# Left to Right
+print(w[-1:-7:-2])
+# Right to Left
+print(w[-1::-1])
+# Reverse
+
+
+# String iteration in python
+
+w="welcome To world"
+t=len(w)
+print(t)
+
+for a in range (t):
+    print(w[a])
+
+
+for a in range (t-1,-1,-1):
+    print(w[a])
+
+
+
+
+
