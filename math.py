792ae27ba4dbdc3bbce6b5781b00a078edffa302
Cyber Tech <90408133+cyberdef33@users.noreply.github.com>
2022-07-12 22:29:13 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,37 @@
+# Math Module
+
+import math
+x=11.4
+print(math.ceil(x))
+# return one more element likas as x=11.4 output is 12
+
+y=-10
+print(math.fabs(y))
+# return postive value 10.0
+
+z=4
+print(math.factorial(z))
+# find factorial
+
+a=16
+print(math.sqrt(a))
+# find quareroot
+
+c=10.5
+print(math.floor(c))
+# return integer value
+
+l=[10,20,30,50]
+print(math.fsum(l))
+
+
+# Random Module 
+
+import random
+
+# randint()
+n=random.randint(2,6)
+print(n)
+
+
+
