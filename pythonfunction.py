4d36c2112e2525c64efc983e9dd41fbbf6b4ec34
Cyber Tech <90408133+cyberdef33@users.noreply.github.com>
2022-07-12 18:24:24 +0530

    Add files via upload

---
+++ pythonfunction.py
@@ -0,0 +1,29 @@
+# Inbulid function & User defined function
+# simeple fun
+# fun with arrgument fun
+# return type fun
+
+# simple function
+
+def sum():
+    print("Hello hacker's")
+
+sum()
+
+# with arrgument function
+
+def add(a,b):
+    print(a+b)
+    print(a*b)
+    print(a-b)
+    print(a/b)
+
+add(70,50)
+
+# return type function
+
+def mul(x,y):
+    c=x+y
+    return c
+var=mul(2,5)
+print(var)
