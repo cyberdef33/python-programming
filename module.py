711bb2674a702223db7c2d0c06c9628cf65f994d
Vikas Tech <90408133+defendervk@users.noreply.github.com>
2022-07-15 21:44:02 +0530

    Update module.py

--- module.py
+++ module.py
@@ -1,14 +1,14 @@
 import random
-# n=random.randint(2,8)
-# print(n)
+n=random.randint(2,8)
+print(n)
 # # randint() return number between 2,8
-# n1=random.randrange(2,4)
-# print(n1)
+ n1=random.randrange(2,4)
+ print(n1)
 
 
-# l=[10,20,30,40]
-# x=random.choice(l)
-# print(x)
+ l=[10,20,30,40]
+ x=random.choice(l)
+ print(x)
 
 
 r=random.random()
